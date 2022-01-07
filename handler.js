6ed7a5fd8569ce027819158eebeb057bfcdf249b
Leandro Lima <leandro.lima@oowlish.com>
2022-01-07 17:09:41 -0300

    fix

--- handler.js
+++ handler.js
@@ -1,16 +1,15 @@
 const AWS = require('aws-sdk');
 
-module.exports.getWidget = async (event) => {
+module.exports.getWidget = async (event, context) => {
   const cloudWatch = new AWS.CloudWatch();
-  const widgetDefinition = {
-    MetricWidget: event.queryStringParameters.widgetDefinition,
-  };
-  const widgetImage = await cloudWatch.getMetricWidgetImage(widgetDefinition).promise();
-  
+  const widgetImage = await cloudWatch.getMetricWidgetImage({
+    MetricWidget: event.body,
+  }).promise();
+
   return {
     statusCode: 200,
     headers: {
-      'Content-Type' : 'image/png',
+      // 'Content-Type' : 'image/png',
       'Access-Control-Allow-Origin' : '*',
       'Access-Control-Allow-Methods' : 'POST, GET, PUT, OPTIONS',
       'Access-Control-Allow-Headers' : 'x-api-key'
